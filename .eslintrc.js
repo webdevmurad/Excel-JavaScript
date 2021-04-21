075b8c0ad86eb56c4f21a4e42569d538f60a4d9b
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2021-04-21 10:11:08 +0300

    Customization webpack. (#5)

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: 'google',
+};
