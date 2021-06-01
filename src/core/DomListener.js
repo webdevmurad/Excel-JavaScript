bea918244a6585020b3388c93a65eb7d2be7c053
murad.ars <murad-arslangiraev-1994@mail.ru>
2021-06-01 20:53:43 +0300

    Добавление просшлушки событий.

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,5 +1,25 @@
+import {capitalize} from './utils';
+
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     this.$root = $root;
+    this.listeners = listeners;
   }
+
+  initDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      // Тоже что и addEventListener
+      this.$root.on(listener, this[method].bind(this));
+    });
+  }
+
+  removeDOMListeners() {
+
+  }
+}
+
+// input => onInput
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
 }
